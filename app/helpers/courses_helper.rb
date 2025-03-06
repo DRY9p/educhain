6ac2bceb682559d61bfcd3a38447de73e093c571
Alex <alburton32@gmail.com>
2025-03-06 21:11:27 +0500

    feat(enrollee)
    Добавлен метод before_render

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -3,4 +3,8 @@ module CoursesHelper
     count = (course.price_cents / 4).round
     tag.p "#{count} enrollees so far"
   end
+
+  def star_icon
+    image_tag("/assets/star.png", width: "100%")
+  end
 end
