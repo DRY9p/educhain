79c6fa2faade9fc27b8a7b664ef2ac975a0c8c29
Alex <alburton32@gmail.com>
2025-03-06 10:46:57 +0500

    chore(db) заполение данными seeds для Course

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_03_082751) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_06_035327) do
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.decimal "price"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+# Очистка данных внесенных для проверки атрибутов модели Course
+Course.destroy_all
+
+100.times do
+  Course.create!(
+    title: Faker::Educator.course_name,
+    price: (rand(9000..100000).to_f)/10,
+    location: Faker::Address.street_address
+  )
+end
