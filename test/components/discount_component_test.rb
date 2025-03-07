a59f12c3855d06dd0db7ad4f73c0be02e783e2d1
Alex <alburton32@gmail.com>
2025-03-07 19:13:24 +0500

    feat: Добавить тесты для DiscountComponent
    - Реализован тест test_render_component с проверкой рендеринга компонента
    - Добавлены ассерты для проверки текста и CSS-классов
    - Исправлена ошибка передачи строки вместо объекта курса
    - Добавлен пример теста с созданием курса через Course.create
    - Обновлены ассерты assert_selector и assert_text

--- test/components/discount_component_test.rb
+++ test/components/discount_component_test.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
 require "test_helper"
-
+require "view_component/test_case"
 class DiscountComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(DiscountComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  # def test_render_component
+  #   render_inline(DiscountComponent.new(item: "my item"))
+  # end
+
+  # Создаем курс с валидными атрибутами
+  def test_render_component
+    course = Course.create!(title: 'Organizing your Time', price_cents: 15500, location: 'London')
+    render_inline(DiscountComponent.new(item: course))
+    assert_selector 'p[class="green"]'
+    assert_text "10% discount"
+  end
+
+  def test_component_not_rendered
+    course = Course.create!(title: 'Organizing your Time', price_cents: 155, location: 'London')
+    render_inline(DiscountComponent.new(item: course))
+    refute_component_rendered
   end
 end
